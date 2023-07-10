fa5336db139309622e7ecec0fb78b6853e12daa7
Dishoungh <dishounghwhiteii@gmail.com>
2023-07-09 22:16:16 -0500

    Slight modifications

--- src/game.rs
+++ src/game.rs
@@ -6,30 +6,35 @@ use rand::{thread_rng, Rng};
 use crate::draw::{draw_block, draw_rectangle};
 use crate::snake::{Direction, Snake};
 
-const FOOD_COLOR: Color = [0.80, 0.00, 0.00, 1.0];
-const BORDER_COLOR: Color = [0.00, 0.00, 0.00, 1.0];
-const GAMEOVER_COLOR: Color = [0.90, 0.00, 0.00, 0.5];
+const FOOD_COLOR: Color = [1.00, 0.00, 0.00, 1.0]; //Red Colored Food
+const BORDER_COLOR: Color = [0.00, 0.00, 0.00, 1.0]; //Black Colored Walls
+const GAMEOVER_COLOR: Color = [0.00, 0.00, 1.00, 0.5]; //Blue Gameover Screen with 50% Opacity
 
 const MOVING_PERIOD: f64 = 0.1;
 const RESTART_TIME: f64 = 1.0;
 
-pub struct Game {
+pub struct Game
+{
+    //Player
     snake: Snake,
-
+    //Food
     food_exists: bool,
     food_x: i32,
     food_y: i32,
-
+    //Dimensions
     width: i32,
     height: i32,
-
+    //Status Parameters
     game_over: bool,
     waiting_time: f64,
 }
 
-impl Game {
-    pub fn new(width: i32, height: i32) -> Game {
-        Game {
+impl Game //Implementation of the Game Struct (basic OOP (sorta))
+{
+    pub fn new(width: i32, height: i32) -> Game
+    {
+        Game
+        {
             snake: Snake::new(2, 2),
             waiting_time: 0.0,
             food_exists: true,
@@ -41,88 +46,112 @@ impl Game {
         }
     }
 
-    pub fn key_pressed(&mut self, key: Key) {
-        if self.game_over {
+    pub fn key_pressed(&mut self, key: Key)
+    {
+        if (self.game_over)
+        {
             return;
         }
 
-        let dir = match key {
+        let dir = match key
+        {
             Key::Up => Some(Direction::Up),
             Key::Down => Some(Direction::Down),
             Key::Left => Some(Direction::Left),
             Key::Right => Some(Direction::Right),
-            _ => Some(self.snake.head_direction()),
+            _ => Some(self.snake.head_direction()), //Looks like "space key" doesn't do anything
         };
 
-        if let Some(dir) = dir {
-            if dir == self.snake.head_direction().opposite() {
+        if let Some(dir) = dir
+        {
+            if (dir == self.snake.head_direction().opposite()) //Can't go opposite of the head direction inward the snake
+            {
                 return;
             }
         }
 
+        //Update Position with new direction
         self.update_snake(dir);
     }
 
-    pub fn draw(&self, con: &Context, g: &mut G2d) {
-        self.snake.draw(con, g);
+    pub fn draw(&self, context: &Context, G: &mut G2d)
+    {
+        self.snake.draw(context, G);
 
-        if self.food_exists {
-            draw_block(FOOD_COLOR, self.food_x, self.food_y, con, g);
+        if (self.food_exists)
+        {
+            draw_block(FOOD_COLOR, self.food_x, self.food_y, context, G);
         }
 
-        draw_rectangle(BORDER_COLOR, 0, 0, self.width, 1, con, g);
-        draw_rectangle(BORDER_COLOR, 0, self.height - 1, self.width, 1, con, g);
-        draw_rectangle(BORDER_COLOR, 0, 0, 1, self.height, con, g);
-        draw_rectangle(BORDER_COLOR, self.width - 1, 0, 1, self.height, con, g);
+        //Draw black walls around corners
+        draw_rectangle(BORDER_COLOR, 0, 0, self.width, 1, context, G);
+        draw_rectangle(BORDER_COLOR, 0, self.height - 1, self.width, 1, context, G);
+        draw_rectangle(BORDER_COLOR, 0, 0, 1, self.height, context, G);
+        draw_rectangle(BORDER_COLOR, self.width - 1, 0, 1, self.height, context, G);
 
-        if self.game_over {
-            draw_rectangle(GAMEOVER_COLOR, 0, 0, self.width, self.height, con, g);
+        //Draw Gameover Block
+        if (self.game_over)
+        {
+            draw_rectangle(GAMEOVER_COLOR, 0, 0, self.width, self.height, context, G);
         }
     }
 
-    pub fn update(&mut self, delta_time: f64) {
+    pub fn update(&mut self, delta_time: f64)
+    {
         self.waiting_time += delta_time;
 
-        if self.game_over {
-            if self.waiting_time > RESTART_TIME {
+        if (self.game_over)
+        {
+            if (self.waiting_time > RESTART_TIME)
+            {
                 self.restart();
             }
+            
             return;
         }
 
-        if !self.food_exists {
+        if !(self.food_exists)
+        {
             self.add_food();
         }
 
-        if self.waiting_time > MOVING_PERIOD {
+        if (self.waiting_time > MOVING_PERIOD)
+        {
             self.update_snake(None);
         }
     }
 
-    fn check_eating(&mut self) {
+    fn check_eating(&mut self)
+    {
         let (head_x, head_y): (i32, i32) = self.snake.head_position();
-        if self.food_exists && self.food_x == head_x && self.food_y == head_y {
+        if (self.food_exists && (self.food_x == head_x) && (self.food_y == head_y))
+        {
             self.food_exists = false;
-            self.snake.restore_tail();
+            self.snake.add_tail();
         }
     }
 
-    fn check_if_snake_alive(&self, dir: Option<Direction>) -> bool {
+    fn check_if_snake_alive(&self, dir: Option<Direction>) -> bool
+    {
         let (next_x, next_y) = self.snake.next_head(dir);
 
-        if self.snake.overlap_tail(next_x, next_y) {
+        if (self.snake.overlap_tail(next_x, next_y)) //If the snake collides into itself, it dies
+        {
             return false;
         }
 
-        next_x > 0 && next_y > 0 && next_x < self.width - 1 && next_y < self.height - 1
+        return ((next_x > 0) && (next_y > 0) && (next_x < (self.width - 1)) && (next_y < (self.height - 1))); //Is the snake's next position within bounds?
     }
 
-    fn add_food(&mut self) {
+    fn add_food(&mut self)
+    {
         let mut rng = thread_rng();
 
         let mut new_x = rng.gen_range(1..self.width - 1);
         let mut new_y = rng.gen_range(1..self.height - 1);
-        while self.snake.overlap_tail(new_x, new_y) {
+
+        while self.snake.overlap_tail(new_x, new_y)
+        {
             new_x = rng.gen_range(1..self.width - 1);
             new_y = rng.gen_range(1..self.height - 1);
         }
@@ -132,17 +161,23 @@ impl Game {
         self.food_exists = true;
     }
 
-    fn update_snake(&mut self, dir: Option<Direction>) {
-        if self.check_if_snake_alive(dir) {
+    fn update_snake(&mut self, dir: Option<Direction>)
+    {
+        if self.check_if_snake_alive(dir)
+        {
             self.snake.move_forward(dir);
             self.check_eating();
-        } else {
+        }
+        else 
+        {
             self.game_over = true;
         }
+
         self.waiting_time = 0.0;
     }
 
-    fn restart(&mut self) {
+    fn restart(&mut self)
+    {
         self.snake = Snake::new(2, 2);
         self.waiting_time = 0.0;
         self.food_exists = true;
--- src/snake.rs
+++ src/snake.rs
@@ -4,18 +4,23 @@ use piston_window::types::Color;
 
 use crate::draw::draw_block;
 
-const SNAKE_COLOR: Color = [0.00, 0.80, 0.00, 1.0];
+const SNAKE_COLOR: Color = [0.00, 1.00, 0.00, 1.0]; //Green Snake
 #[derive(Copy, Clone, PartialEq)]
-pub enum Direction {
+
+pub enum Direction 
+{
     Up,
     Down,
     Left,
     Right,
 }
 
-impl Direction {
-    pub fn opposite(&self) -> Direction {
-        match *self {
+impl Direction
+{
+    pub fn opposite(&self) -> Direction 
+    {
+        match *self
+        {
             Direction::Up => Direction::Down,
             Direction::Down => Direction::Up,
             Direction::Left => Direction::Right,
@@ -23,97 +28,122 @@ impl Direction {
         }
     }
 }
+
 #[derive(Debug, Clone)]
-struct Block {
+struct Block
+{
     x: i32,
     y: i32,
 }
 
-pub struct Snake {
+pub struct Snake
+{
     direction: Direction,
     body: LinkedList<Block>,
     tail: Option<Block>,
 }
 
-impl Snake {
-    pub fn new(x: i32, y: i32) -> Snake {
+impl Snake
+{
+    pub fn new(x: i32, y: i32) -> Snake
+    {
         let mut body: LinkedList<Block> = LinkedList::new();
+        
         body.push_back(Block {
             x: x + 2,
             y,
         });
+        
         body.push_back(Block {
             x: x + 1,
             y,
         });
+        
         body.push_back(Block {
             x,
             y,
         });
 
-        Snake {
+        Snake 
+        {
             direction: Direction::Right,
             body,
             tail: None,
         }
     }
 
-    pub fn draw(&self, con: &Context, g: &mut G2d) {
-        for block in &self.body {
-            draw_block(SNAKE_COLOR, block.x, block.y, con, g);
+    pub fn draw(&self, context: &Context, G: &mut G2d)
+    {
+        for block in &self.body
+        {
+            draw_block(SNAKE_COLOR, block.x, block.y, context, G);
         }
     }
 
-    pub fn head_position(&self) -> (i32, i32) {
+    pub fn head_position(&self) -> (i32, i32)
+    {
         let head_block = self.body.front().unwrap();
         (head_block.x, head_block.y)
     }
 
-    pub fn move_forward(&mut self, dir: Option<Direction>) {
-        match dir {
+    pub fn move_forward(&mut self, dir: Option<Direction>)
+    {
+        match dir
+        {
             Some(d) => self.direction = d,
             None => (),
         }
 
         let (last_x, last_y): (i32, i32) = self.head_position();
 
-        let new_block = match self.direction {
-            Direction::Up => Block {
+        let new_block = match self.direction 
+        {
+            Direction::Up => Block
+            {
                 x: last_x,
                 y: last_y - 1,
             },
-            Direction::Down => Block {
+            Direction::Down => Block
+            {
                 x: last_x,
                 y: last_y + 1,
             },
-            Direction::Left => Block {
+            Direction::Left => Block
+            {
                 x: last_x - 1,
                 y: last_y,
             },
-            Direction::Right => Block {
+            Direction::Right => Block
+            {
                 x: last_x + 1,
                 y: last_y,
             },
         };
+        
         self.body.push_front(new_block);
         let removed_block = self.body.pop_back().unwrap();
         self.tail = Some(removed_block);
     }
 
-    pub fn head_direction(&self) -> Direction {
+    pub fn head_direction(&self) -> Direction
+    {
         self.direction
     }
 
-    pub fn next_head(&self, dir: Option<Direction>) -> (i32, i32) {
+    pub fn next_head(&self, dir: Option<Direction>) -> (i32, i32)
+    {
         let (head_x, head_y): (i32, i32) = self.head_position();
 
         let mut moving_dir = self.direction;
-        match dir {
+        
+        match dir
+        {
             Some(d) => moving_dir = d,
             None => {}
         }
 
-        match moving_dir {
+        match moving_dir
+        {
             Direction::Up => (head_x, head_y - 1),
             Direction::Down => (head_x, head_y + 1),
             Direction::Left => (head_x - 1, head_y),
@@ -121,23 +151,30 @@ impl Snake {
         }
     }
 
-    pub fn restore_tail(&mut self) {
+    pub fn add_tail(&mut self)
+    {
         let blk = self.tail.clone().unwrap();
         self.body.push_back(blk);
     }
 
-    pub fn overlap_tail(&self, x: i32, y: i32) -> bool {
+    pub fn overlap_tail(&self, x: i32, y: i32) -> bool
+    {
         let mut ch = 0;
-        for block in &self.body {
-            if x == block.x && y == block.y {
+        for block in &self.body
+        {
+            if ((x == block.x) && (y == block.y))
+            {
                 return true;
             }
 
             ch += 1;
-            if ch == self.body.len() - 1 {
+            
+            if (ch == (self.body.len() - 1))
+            {
                 break;
             }
         }
+
         return false;
     }
 }
